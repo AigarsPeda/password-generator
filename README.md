e95d4d3ce38ab3983fa8f6526adc33fde523313a
Aigars Peda <aigarspeda@gmail.com>
2020-03-09 09:08:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# react-typescript-babel-webpack-sass-boilerplate
+
+## Helpful article
+
+https://medium.com/@dahvinchee/how-to-set-up-a-react-typescript-project-from-scratch-with-babel-and-webpack-6b069881229d
+
+## Download dependencies
+
+npm i
+
+## Scripts
+
+npm start
---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../styles/index.css";
+import "../styles/styles.scss";
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2>SASS</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
