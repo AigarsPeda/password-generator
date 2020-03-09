be34f18c2194cb764f094d82e8a1e5e63a944e69
Aigars Peda <aigarspeda@gmail.com>
2020-03-09 13:41:20 +0200

    A

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 
-import "../styles/index.css";
 import "../styles/styles.scss";
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-        <h2>SASS</h2>
-      </div>
-    );
-  }
-}
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>SASS</h2>
+    </div>
+  );
+};
 
 export default App;
